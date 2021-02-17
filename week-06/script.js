9e161c9d04b90d34c9361d8f2df924e29ee47983
anirudhchandar <73598068+anirudhchandar@users.noreply.github.com>
2021-02-16 20:37:21 -0500

    Create script.js

---
+++ week-06/script.js
@@ -0,0 +1,23 @@
+const fruits = ['apples','mango','grape', 'banana'];
+
+for(let i=0;i<fruits.length; i++)
+{let fruit = fruits[i];
+}
+
+
+
+
+if(age < 8)
+{
+    console.log("Check out the Merry-Go-Round. You'll love it!");
+} 
+else if(age > 8 && age < 65 && height > 4.5)
+{
+console.log("Check out the Roller Coaster. It's awesome!");
+}
+else 
+{
+    console.log('Why not enjoy a float down the Lazy River?');
+}
+
+var noun = 
